0b4e79d11651b9c5162032f0311d9f6d1cf12d7a
Tribex <joshua@derfers.us>
2015-06-22 22:07:34 +0800

    Fixed? share URLs

--- out/scripts/manage-social-buttons.js
+++ out/scripts/manage-social-buttons.js
@@ -1,6 +1,6 @@
 // Replaces the relative URLs in share buttons with absolute ones.
 $(document).ready(function() {
   $('.sharebtn').each(function() {
-    $(this).attr("href", $(this).attr("href").split("--[LOCATION]--").join(window.location));
+    $(this).attr("href", $(this).attr("href").split("--[LOCATION]--").join(encodeURIComponent(window.location)));
   });
 });
